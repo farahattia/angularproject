47e3ebd826744ba509996a67615504d97d5657c4
farahattia <40789656+farahattia@users.noreply.github.com>
2019-03-31 16:16:07 +0200

    first commit

---
+++ src/app/connexion/inscription/inscription.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+  MinLengthValidator
+  } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-inscription',
+  templateUrl: './inscription.component.html',
+  styleUrls: ['./inscription.component.css']
+})
+export class InscriptionComponent implements OnInit {
+  myform: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.myform=new FormGroup(
+      {
+        name:new FormControl('',Validators.required),
+        email:new FormControl('',[Validators.required,Validators.email]),
+        password:new FormControl('',[Validators.required,Validators.minLength(8)])
+      }
+
+
+    )
+  }
+
+}
